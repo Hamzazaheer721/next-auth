50513e932bfb1fec340dbc7bb82662ff553daa24
Hamza Zaheer <hamzazaheer721@gmail.com>
2024-01-23 15:50:23 +0500

    next-auth: login/signup create

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,51 @@
-const LoginPage = () => {
+"use client"
+
+import Link from "next/link"
+import { ChangeEvent, MouseEvent, useState } from "react"
+
+export default function Login() {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setState((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {}
+
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center min-h-screen py-2'>
       <h1>Login</h1>
+      <br />
+      <label htmlFor='email'>Email</label>
+      <input
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600'
+        id='email'
+        type='text'
+        name='email'
+        onChange={handleChange}
+        placeholder='email'
+      />
+      <br />
+      <label htmlFor='password'>Password</label>
+      <input
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600'
+        id='Password'
+        type='text'
+        name='password'
+        onChange={handleChange}
+        placeholder='password'
+      />
+      <br />
+      <button
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600 bg-red-500'
+        onClick={handleClick}
+      >
+        Login
+      </button>
+      <br />
+      <Link href='/signup'> Visit Signup Page</Link>
     </div>
   )
 }
-
-export default LoginPage
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useState } from "react"
+import Link from "next/link"
+import { ChangeEvent, MouseEvent, useState } from "react"
 
 export default function Signup() {
   const [state, setState] = useState({
@@ -9,19 +10,53 @@ export default function Signup() {
     username: ""
   })
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setState((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {}
+
   return (
     <div className='flex flex-col justify-center items-center min-h-screen py-2'>
       <h1>Sign up</h1>
-      <hr />
+      <br />
       <label htmlFor='username'>User Name</label>
       <input
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600'
+        id='username'
         type='text'
         name='username'
-        onChange={(e) =>
-          setState((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-        }
+        onChange={handleChange}
         placeholder='username'
       />
+      <br />
+      <label htmlFor='email'>Email</label>
+      <input
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600'
+        id='email'
+        type='text'
+        name='email'
+        onChange={handleChange}
+        placeholder='email'
+      />
+      <br />
+      <label htmlFor='password'>Password</label>
+      <input
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600'
+        id='Password'
+        type='text'
+        name='password'
+        onChange={handleChange}
+        placeholder='password'
+      />
+      <br />
+      <button
+        className='p-2 border-gray-300 rounded-lg-mb-4 focus:outline-none focus:border-gray-600 bg-red-500'
+        onClick={handleClick}
+      >
+        Signup
+      </button>
+      <br />
+      <Link href='/login'> Visit Login Page</Link>
     </div>
   )
 }
